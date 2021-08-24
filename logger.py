c8d34f9371d763eb5347426501fe90f220ac3479
pratik-lal <pratik@hotmail.com>
2021-08-24 20:26:09 +0530

    logger function - file rotation updated

--- logger.py
+++ logger.py
@@ -1,11 +1,13 @@
 import logging
+from logging import handlers
 
 # Create logger
 logger = logging.getLogger("synthetic events")
 logger.setLevel(logging.DEBUG)
 
 # File handler
-fh = logging.FileHandler(filename="log/agent.log", mode="a")
+# fh = logging.FileHandler(filename="log/agent.log", mode="a")
+fh = logging.handlers.RotatingFileHandler(filename="log/agent.log", mode="a", maxBytes=1000000, backupCount=10)
 fh.setLevel(logging.DEBUG)
 
 # log file formatter
@@ -13,6 +15,6 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message
 fh.setFormatter(formatter)
 
 logger.addHandler(fh)
-# logger.info("hello logger!")
+# logger.info("hello logger! hello logger! hello logger! hello logger!")
 # print(logger)
 
